e3fa81e7e6a2ccc5fcc9f6f503965531056d5724
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-04-23 14:56:06 +0100

    Initialise mult. logistic regression file

---
+++ glm.R
@@ -0,0 +1,14 @@
+# Multiple Logistic Regression
+
+# This R scripit attempts to apply multiple logistic regression to predict fatal accidents
+
+
+
+# Load history
+loadhistory()
+# Clear workspace
+rm(list=ls())
+
+# Load data
+accidents = read.csv("prep.csv")
+
