153e154389570ae167fe01e1457571cf138beffa
Isaac Vander Sluis <isaacvandersluis@gmail.com>
2020-04-23 16:38:16 +0100

    Attempt multiple logistic regression

--- glm.R
+++ glm.R
@@ -11,4 +11,20 @@ rm(list=ls())
 
 # Load data
 accidents = read.csv("prep.csv")
+attach(accidents)
+summary(fatal)
+table(fatal)
 
+# Create test set
+test = 1:1000
+train.X = accidents[-test,]
+test.X = accidents[test,]
+train.Y = fatal[-test]
+test.Y = fatal[test]
+
+# Fit multiple logistic regression
+glm.fit = glm(fatal ~ ., data = accidents, family = binomial, subset = -test)
+
+# Make predictions
+glm.probs = predict(glm.fit, accidents[test,], type = "response")
+# Error in model.frame.default
